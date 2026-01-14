bdfceb8a698c245bdd2afa8994ad24502d4a4271
Naphadepriyanka <naphadepriyanka@tamu.edu>
2026-01-14 08:39:52 -0600

    Tried making some improvements

--- tasktracking.py
+++ tasktracking.py
@@ -5,19 +5,20 @@ db = client["migration_system"]
 
 tasklist = db["tasks"] # each task is an induvidual document
 users = db["users"] # each user is an induvidual document
-
+user_id = ""
 # function to add tasks to the tasks collection 
-def get_input_task ():
+def get_input_task (user_id):
+    #global user_id
     task = input("Please enter a task: ")
-    user_id = input("Please enter user id: ")
     add_task(task, user_id)
 
-# add's a user to the user collection (need to make another function to check if username is taken?)
-# login function?
+# add's a user to the user collection 
 def get_input_user ():
+    
     username = input("Please enter user name: ")
-    user_id = input("Please enter user id:  ")
+    get_id()
     add_user(username, user_id)
+    return user_id
     
     
 
@@ -42,16 +43,16 @@ def add_user (username, user_id):
     
 
 #function that goes through task list and retrives all the tasks for a specific user 
-def find_user_tasks ():
-    user_id = input("Please enter your user id: ")
+def find_user_tasks (user_id):
+    #user_id = input("Please enter your user id: ")
     finduser = db.tasks.find({"user_id" : user_id })
     for i in finduser:
         print(i)
 
 
 #function to mark a task complete or incomplete 
-def update_task_status():
-    user_id = input("Please enter your user id: ")
+def update_task_status(user_id):
+   # user_id = input("Please enter your user id: ")
     #find way to do this without entering user id twice
     find_user_tasks()
     task = input("What is the name of the task you'd like to update to show compleation: ")
@@ -77,11 +78,24 @@ def count_compleation(user_id):
     
 
 # function to display user profile 
-def display_profile():
-    user_id = input("Please enter your user id: ")
+def display_profile(user_id):
+    #global user_id
+    #user_id = input("Please enter your user id: ")
     profile = db.users.find({"user_id":user_id})
     for i in profile:
         pprint.pprint(i)
+   
+
+# improvements 
+#get user id function 
+def get_id():
+    global user_id
+    user_id = input("Please enter your user id: ")
+    return user_id
+
+
+
+# Possible additions: Log in function, verify that user_id is unique
 
 def menu():
     a = input("would you like to create an account enter yes or no: ")
@@ -89,19 +103,22 @@ def menu():
     if a == "yes":
             get_input_user()
             
+            
+            
     else:
-            display_profile()
+            display_profile(get_id())
+            
             
     while True:
         a = input("What would you like to do A: add a task, B: update the status of a task, C: View your tasks, D: View your profile, or E: Exit program . Please enter the corresponding letter of what you'd like to do:  ")
         if a == "A":
-            get_input_task()
+            get_input_task(user_id)
         elif a == "B":
-            update_task_status()
+            update_task_status(user_id)
         elif a == "C":
-            find_user_tasks()
+            find_user_tasks(user_id)
         elif a == "D":
-            display_profile()
+            display_profile(user_id)
         elif a == "E":
             break
 
